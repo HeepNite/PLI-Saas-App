75f307e856c31974f53041efec19c69e37ae50e7
Mariano Barrionuevo <devocamptuc@gmail.com>
2025-10-12 14:33:32 -0400

    Refactoring search input // adding a search page with courses

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,9 @@
 import type {Metadata} from "next";
-import {Bricolage_Grotesque} from "next/font/google";
 
 import "./globals.css";
 import React from "react";
 import {ThemeProvider} from "next-themes";
 
-const bricolage = Bricolage_Grotesque({
-    variable: "--font-bricolage",
-    subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
     title: "Palladium Latin Institute",
